07002ffca03e5fb7baf1334953e9123fef1659ab
Barry8197 <40179917+Barry8197@users.noreply.github.com>
2018-07-06 12:12:46 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,28 +1,43 @@
 from Functions import eia860,eia923
 import warnings
-from apscheduler.schedulers.background import BackgroundScheduler
+from Constants import Years_eia860, Years_eia923
+import os
+import shutil
+import datetime
 
-scheduler = BackgroundScheduler()
+now = datetime.datetime.now()
+file_name = "Automated_Output.txt"
+output = open(file_name, 'a')
 
-def scheduled_func()
-    print("Begining eia923 imports from years 1970+")
-    try :
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            eia923()
+path = os.getcwd()
+try :
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        eia923()
+        output.write("\n")
 
-    except Exception:
-        print("Database is up to date")
+except Exception:
+    output.write("")
 
-    print("Begining eia860 imports from years 1990+")
-    try :
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            eia860()
+os.chdir(path)
+try :
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        eia860()
+        output.write("Database is up to date\n")
 
-    except Exception:
-        print("Database is up to date")
+except Exception:
+    output.write("Database is up to date\n")
+
+
+try :
+    for year1,year2 in zip(Years_eia860,Years_eia923):
+        os.chdir(path)
+        shutil.rmtree("%s/%s" % (path,year1))
+        shutil.rmtree("%s/%s" % (path,year2))
+
+except Exception :
+    output.write("")
     
-    
-scheduler.add_job(scheduled_func, 'interval', minutes = 1, id='my_job_id')
-scheduler.start()
+output.write("The database was last updated on the %s-%s-%s at %2i:%2i \n" % (now.month, now.day,now.year, now.hour,now.minute))
+output.write("\n")
