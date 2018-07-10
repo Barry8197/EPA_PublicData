4dc01547ed7687dd51d0f1256807d7154a513db7
Barry8197 <40179917+Barry8197@users.noreply.github.com>
2018-07-10 16:20:01 +0100

    Add files via upload

---
+++ Automated_Output_For_PC.py
@@ -0,0 +1,6 @@
+import getpass
+import os
+import sys
+
+username = getpass.getuser()
+os.system('SCHTASKS /Create /SC MONTHLY /MO first /D MON /TN Update_PSQL /TR c:\Users\%s\EPA_PublicData' %(username))
---
+++ Automated_Update_For_MacOS.py
@@ -0,0 +1,20 @@
+import getpass
+import os
+import sys
+
+
+username = getpass.getuser()
+os.chdir('/Users/%s/EPA_PublicData' % (username))
+location = os.getcwd()
+file_name = "__init__.cron"
+output = open(file_name, 'w')
+output.write("0 10 1 * * ")
+path = 'cd %s ;' % (location)
+output.write(path)
+output.write(sys.executable)
+output.write(" __init__.py")
+output.write("\n")
+output.close()
+
+
+os.system('crontab __init__.cron ')
