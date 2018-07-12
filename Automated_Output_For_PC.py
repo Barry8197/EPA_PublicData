25e7ae4af9bd7d0d8a1d826472cbfd4ec0861a6c
Barry8197 <40179917+Barry8197@users.noreply.github.com>
2018-07-12 15:41:06 +0100

    Update Automated_Output_For_PC.py

--- Automated_Output_For_PC.py
+++ Automated_Output_For_PC.py
@@ -3,4 +3,4 @@ import os
 import sys
 
 username = getpass.getuser()
-os.system('SCHTASKS /Create /SC MONTHLY /MO first /D MON /TN Update_PSQL /TR c:\Users\%s\EPA_PublicData' %(username))
+os.system('SCHTASKS /Create /SC MONTHLY /MO first /D MON /TN Update_PSQL /TR c:\\Users\\%s\\EPA_PublicData' %(username))
